b84e7c899af0d5bc4b8530109fd4587b94942596
roofimon <roofimon@gmail.com>
2021-08-25 10:46:12 +0700

    Add new api

--- app.js
+++ app.js
@@ -7,20 +7,34 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json("Hello World!!!");
+app.get("/health-check", (req, res) => {
+  res.json("Alive");
 });
 
-app.get("/:email", (req, res) => {
+app.post("/individual-informations", (req, res) => {
+  let individualDetail = req.body.individualDetail;
+  console.log(individualDetail);
+  individualDetail.submitedDate = new Date();
+  res.send(individualDetail);
+});
+
+app.get("/individual-informations/:email", (req, res) => {
   let email = req.params.email;
   let individual = PaymentService.getIndividualDetail(email);
   res.send(individual);
 });
 
-app.post("/", function (req, res) {
-  const individual = req.body.individual;
-  console.log(individual.email);
-  res.send(individual);
+app.post("/working-records", (req, res) => {
+  let workingRecord = req.body.workingRecord;
+  workingRecord.submitedDate = new Date();
+  console.log(workingRecord);
+  res.send(workingRecord);
+});
+
+app.get("/working-records/:email", (req, res) => {
+  let email = req.params.email;
+  let workingRecord = PaymentService.getIndividualWorkingRecord(email);
+  res.send(workingRecord);
 });
 
 app.listen(port, () => {
