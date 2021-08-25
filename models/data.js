692a008071c960162461211af80d1f7720191b25
roofimon <roofimon@gmail.com>
2021-08-25 10:45:52 +0700

    Add submited date

--- models/data.js
+++ models/data.js
@@ -3,13 +3,17 @@ export const individuals = {
     email: "punyawee_w@odds.team",
     name: "Punyavee",
     tel: "000000000",
+    accontNumber: "12345678",
     dailyRate: 8000,
+    submitedDate: new Date(1995, 11, 17, 3, 24, 0),
   },
   "roof@odds.team": {
     email: "roof@odds.team",
     name: "Twin Panitsombat",
     tel: "000000000",
+    accontNumber: "82345678",
     dailyRate: 2000,
+    submitedDate: new Date(1995, 11, 17, 3, 24, 0),
   },
 };
 
