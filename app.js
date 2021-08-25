1cc7cf92aa124b6ab24992e73a7d8fcc11adb037
roofimon <roofimon@gmail.com>
2021-08-25 09:54:14 +0700

    Using express json instead of body parser

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
 import * as PaymentService from "./models/paymentService.js";
 
 const port = 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Hello World!!!");
