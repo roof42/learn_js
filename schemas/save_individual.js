5838e43a63508f00e3161589a4c415aff5561da3
roov <roof@odds.team>
2021-08-26 14:57:29 +0700

    Change kitty name

--- schemas/save_individual.js
+++ schemas/save_individual.js
@@ -7,9 +7,9 @@ mongoose.connect("mongodb://localhost:27017/test", {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
-const silence = new Model.Kitten({ name: "Silence" });
-silence.save(function (err, fluffy) {
+const kitty = new Model.Kitten({ name: "Louve" });
+kitty.save(function (err, fluffy) {
   if (err) return console.error(err);
 });
 
-console.log(silence.name); // 'Silence'
+console.log(kitty.name); // 'Silence'
