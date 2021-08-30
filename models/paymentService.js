f74099c0f67c928edfe66b40a370b622ed6e2ede
roof <roof@odds.team>
2021-08-30 18:15:20 +0700

    Change import

--- models/paymentService.js
+++ models/paymentService.js
@@ -1,10 +1,16 @@
 import * as Database from "./data.js";
-
+import models from "../schemas/index.js";
 export const getIndividualDetail = (emailAddress) => {
   let individualDetail = Database.individuals[emailAddress];
   return individualDetail;
 };
+
 export const getIndividualWorkingRecord = (emailAddress) => {
   let workingRecord = Database.workingRecords[emailAddress];
   return workingRecord;
+};
+
+export const getAllIndividualDetail = async () => {
+  let result = await models.IndividualDetail.find({});
+  return result;
 };
