cb69001d8dd2251d7e521e2469d0b6e07e35c3a9
roofimon <roof@odds.team>
2021-09-10 22:00:17 +0700

    Refactor

--- kafka/producer.js
+++ kafka/producer.js
@@ -2,31 +2,24 @@ import { Kafka } from "kafkajs";
 import config from "./config.js";
 import messages from "./input.js";
 
-const client = new Kafka({
+const producer = new Kafka({
 	brokers: config.kafka.BROKERS,
 	clientId: config.kafka.CLIENTID,
-});
-
-const topic = config.kafka.TOPIC;
-
-const producer = client.producer();
-
-let i = 0;
+}).producer();
 
 const sendMessage = async (producer, topic) => {
 	await producer.connect();
 
-	setInterval(function () {
-		i = i >= messages.length - 1 ? 0 : i + 1;
+	setInterval(() => {
+		let i = Math.floor(Math.random() * 4);
+		let value = JSON.stringify(messages[i]);
 		let payloads = {
 			topic: topic,
-			messages: [
-				{ key: "coronavirus-alert", value: JSON.stringify(messages[i]) },
-			],
+			messages: [{ key: "coronavirus-alert", value: value }],
 		};
 		console.log("payloads=", payloads);
 		producer.send(payloads);
 	}, 5000);
 };
 
-sendMessage(producer, topic);
+sendMessage(producer, config.kafka.TOPIC);
