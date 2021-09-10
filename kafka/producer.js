127e1a79a7133479f4a453177a054a21552e92a3
roofimon <roof@odds.team>
2021-09-10 20:24:42 +0700

    Extract sample data

--- kafka/producer.js
+++ kafka/producer.js
@@ -1,10 +1,10 @@
 import { Kafka } from "kafkajs";
 import config from "./config.js";
-import messages from "./input";
+import messages from "./input.js";
 
 const client = new Kafka({
-  brokers: config.kafka.BROKERS,
-  clientId: config.kafka.CLIENTID,
+	brokers: config.kafka.BROKERS,
+	clientId: config.kafka.CLIENTID,
 });
 
 const topic = config.kafka.TOPIC;
@@ -14,19 +14,19 @@ const producer = client.producer();
 let i = 0;
 
 const sendMessage = async (producer, topic) => {
-  await producer.connect();
+	await producer.connect();
 
-  setInterval(function () {
-    i = i >= messages.length - 1 ? 0 : i + 1;
-    let payloads = {
-      topic: topic,
-      messages: [
-        { key: "coronavirus-alert", value: JSON.stringify(messages[i]) },
-      ],
-    };
-    console.log("payloads=", payloads);
-    producer.send(payloads);
-  }, 5000);
+	setInterval(function () {
+		i = i >= messages.length - 1 ? 0 : i + 1;
+		let payloads = {
+			topic: topic,
+			messages: [
+				{ key: "coronavirus-alert", value: JSON.stringify(messages[i]) },
+			],
+		};
+		console.log("payloads=", payloads);
+		producer.send(payloads);
+	}, 5000);
 };
 
 sendMessage(producer, topic);
